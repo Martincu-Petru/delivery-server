48c00f64093b39a93a08a239f1bc63ef52459a28
Marțincu Petru <martincu.petru@gmail.com>
2019-05-25 16:21:59 +0300

    Added User class

--- project/main.py
+++ project/main.py
@@ -1,6 +1,7 @@
 import logging
 import json
 import os
+import uuid
 
 from flask_cors import CORS
 from flask import Flask, request
@@ -9,10 +10,29 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://database-user:1234@/database_scheme'
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-# db = SQLAlchemy(app)
+
+class Users(db.Model):
+    user_id = db.Column(db.String(36), primary_key=True, unique=True, nullable=False)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(30), nullable=False)
+    phone_number = db.Column(db.String(10), nullable=False)
+    address = db.Column(db.String(50), nullable=False)
+
+    def __init__(self, first_name, last_name, password, email, phone_number, address):
+        self.user_id = str(uuid.uuid4())
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
+        self.email = email
+        self.phone_number = phone_number
+        self.address = address
 
 
 @app.errorhandler(500)
